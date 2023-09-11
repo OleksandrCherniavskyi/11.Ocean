d865e549e370301836372ebe70d92a93c6ce52d2
OleksandrCherniavskyi <105165580+OleksandrCherniavskyi@users.noreply.github.com>
2023-09-11 10:23:27 +0200

    - add images

--- ocean/sea/views.py
+++ ocean/sea/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UploadForm
+from django.http import HttpResponse
 
-# Create your views here.
+def origin_image_view(request):
+    if request.method == 'POST':
+        form = UploadForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            form.save()
+            return redirect('success')
+    else:
+        form = UploadForm()
+    return render(request, {'form': form})
+
+
+def success(request):
+    return HttpResponse('successfully uploaded')
